a389e1ba91331552163530b7a92ba9a753b98915
xr2d2 <xvinland@gmail.com>
2023-11-30 22:27:55 -0600

    arths

--- index.js
+++ index.js
@@ -205,3 +205,17 @@ Bg3.addEventListener('click' , () => {
       //remove customized changes from local storage
       changeBG();
 });
+document.onkeydown = function(e) {
+      if(event.keyCode == 123) {
+      return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+      return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+      return false;
+      }
+      if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+      return false;
+      }
+      }
